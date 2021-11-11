c1d6f5520d485238a3d92b7e9bc52e01eb5de4a8
PatrickNiccolai2 <patrick.m.niccolai.gr@dartmouth.edu>
2021-11-11 17:57:12 -0500

    test

--- app/src/main/java/com/dartmouth/moonshot/SocialsActivity.kt
+++ app/src/main/java/com/dartmouth/moonshot/SocialsActivity.kt
@@ -1,7 +1,11 @@
 package com.dartmouth.moonshot
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SocialsActivity: AppCompatActivity() {
-    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 }
